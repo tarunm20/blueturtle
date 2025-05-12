c76764233c3fb1ab0eaabb7f8250d06714a07e34
Murugan, Tarun <tarun.murugan24@gmail.com>
2025-05-12 12:59:19 -0700

    github hotfix

--- frontend/apps/web/app/(marketing)/layout.tsx
+++ frontend/apps/web/app/(marketing)/layout.tsx
@@ -3,8 +3,11 @@ import { getSupabaseServerClient } from '@kit/supabase/server-client';
 import { SiteFooter } from '~/(marketing)/_components/site-footer';
 import { SiteHeader } from '~/(marketing)/_components/site-header';
 import { withI18n } from '~/lib/i18n/with-i18n';
+import { cookies } from 'next/headers';
 
 async function SiteLayout(props: React.PropsWithChildren) {
+  const cookieStore = await cookies();
+  cookieStore.getAll();
   const client = getSupabaseServerClient();
 
   const {
